9c1658dca9cb1fb97512d9d3112fc3981c5a4b92
Abduljabbar-Alhamid <abduljabbaralhamid@gmail.com>
2026-04-19 07:09:04 +0300

    d
    update the caluclator

--- js/main.js
+++ js/main.js
@@ -4,6 +4,55 @@ const specialChars = ["%", "*", "/", "-", "+", "="];
 let output = "";
 //Define fuction to calculate based on button ckicked
 const calclate = (btnValue) => {
+  // factorial 
+  const factorial = (n) => {
+    if(n< 0) return NaN;
+    if(n===0)return 1;
+    let res = 1; 
+    for(let i; i <= n; i++)
+    {
+      res *= i;
+
+    }
+    return res;
+  }
+
+  const transformExpression = (expr) => {
+    return (
+      expr
+        // constants
+        .replace(/π/g, "Math.PI")
+        .replace(/\be\b/g, "Math.E")
+
+        // sqrt
+        .replace(/√\((.*?)\)/g, "Math.sqrt($1)")
+
+        // power x^y
+        .replace(/(\d+)\^(\d+)/g, "Math.pow($1,$2)")
+
+        // square
+        .replace(/(\d+)²/g, "Math.pow($1,2)")
+
+        // log
+        .replace(/log\((.*?)\)/g, "Math.log10($1)")
+
+        // ln
+        .replace(/ln\((.*?)\)/g, "Math.log($1)")
+
+        // abs
+        .replace(/\|(.*?)\|/g, "Math.abs($1)")
+
+        // exp
+        .replace(/exp\((.*?)\)/g, "Math.exp($1)")
+
+        // factorial
+        .replace(/(\d+)!/g, "factorial($1)")
+
+        // percentage
+        .replace(/%/g, "/100")
+    );
+  };
+
   display.focus();
   if (btnValue === "=" && output !== "") {
     //If out has '%', replace with '/100' before evaluating.
